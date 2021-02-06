229ba8d1b0214de8c0ada52459402faacb611ded
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2021-02-06 21:52:26 +0900

    練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './components/Login'
 
-function App() {
+// pushの練習
+
+const App: React.FC<{}> = () => {
+  const [count, setCount] = useState<number>(0);
+  const handleClick = () => {
+    setCount(count + 1)
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App.tsx</h1>
+      <Login id="1" name="maaya" age={28}
+        isLive={true} />
+      {count}
+      <button onClick={handleClick} >count up</button>
+
+
+
+
     </div>
+
   );
-}
+};
 
 export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NamedTupleMember } from 'typescript'
+import { UserType } from '../types/UserType';
+
+
+
+const Login: React.FC<UserType> = () => {
+    return (
+        <div>
+            <h1>login</h1>
+        </div>
+    )
+}
+
+export default Login
